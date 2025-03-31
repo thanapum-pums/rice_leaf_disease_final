3f3f67fb1bdb3bd2890ae6333b49939d494010c8
thanapum-pums <thanapum.pums@bumail.net>
2025-03-31 21:40:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,13 +54,7 @@ def main():
         - โรคกาบใบแห้ง (Sheath Blight)
         - ใบข้าวสุขภาพดี (Healthy Rice Leaf)
         """)
-     with col2:
-        st.write("""
-        - โรคใบวงสีน้ำตาล (Leaf Scald)
-        - โรคกาบใบแห้ง (Sheath Blight)
-        - ใบข้าวสุขภาพดี (Healthy Rice Leaf)
-        """)
-
+     
         uploaded_file = st.file_uploader("เลือกอัพโหลดรูปใบข้าว", type=["jpg", "jpeg", "png"])
 
         if uploaded_file is not None:
